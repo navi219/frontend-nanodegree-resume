7e5a6d2e9adb7a2124759f35eb223c93337dbfc1
Daniel Maron <maron.daniel@gmail.com>
2016-12-14 14:24:04 -0500

    Practice replace method

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,4 +3,6 @@ This is empty on purpose! Your code to build the resume will go here.
  */
  $("#main").append(["Daniel Maron"]);
 
- var awesomeThoughts = "This here's Lucille, and she is AWESOME."
+ var awesomeThoughts = "This here's Lucille, and she is AWESOME.";
+
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
