4de2e2eacf2de7462729c6482aeb26fb15ab1a35
Daniel Maron <maron.daniel@gmail.com>
2016-12-14 14:17:52 -0500

    Add my name and note about Lucille

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ $("#main").append(["Daniel Maron"]);
+
+ var awesomeThoughts = "This here's Lucille, and she is AWESOME."
