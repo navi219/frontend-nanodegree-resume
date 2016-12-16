58c24f5da2e3aed47af924e218964308bfd12a8b
Daniel Maron <maron.daniel@gmail.com>
2016-12-16 09:30:09 -0500

    Add work and education objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,14 +5,26 @@ var formattedbioPic = HTMLbioPic.replace("%data%", "images/fry.jpg")
 var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", "I'm the One the prophecy foretold.")
 var formattedSkills = HTMLskills.replace("%data%", "This, That, and the Other Things")
 
-var bio = { name: "Daniel Maron",
-	role: "IT Dilettante",
-	email: "maron.daniel@gmail.com",
-	bioPic: "images/fry.jpg",
-	welcomeMsg: "I'm the One the prophecy foretold.",
-	skills: ["This", "That", "The Other Thing"]
-	}
+var bio = {
+	"name": "Daniel Maron",
+	"role": "IT Dilettante",
+	"email": "maron.daniel@gmail.com",
+	"bioPic": "images/fry.jpg",
+	"welcomeMsg": "I'm the One the prophecy foretold.",
+	"skills": ["This", "That", "The Other Thing"]
+}
 
+var work = {
+	"position": "IT Support Specialist",
+	"employer": "FacelessCorp",
+	"tenure": 13,
+	"city": "Ralleigh"
+}
+
+var education = {}
+	education["name"] = "UNC"
+	education["years"] = "1989-1993"
+	education["city"] = "Springfield"
 
  $("#main").append([bio.name]);
  $("#main").append([bio.role]);
@@ -21,3 +33,5 @@ var bio = { name: "Daniel Maron",
  $("#main").append([bio.welcomeMsg]);
  $("#main").append([bio.skills]);
 
+ $("#main").append(work["position"]);
+ $("#main").append([education.name]);
